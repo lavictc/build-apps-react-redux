ec6355815953cc948e50d13c79020518e26356fb
lavictc <carlos.lavictoire@gmail.com>
2025-11-13 12:34:33 +0000

    initial dev environment setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 function Hi() {
-  return <p>Hi.</p>;
+  return <p>Hi world.</p>;
 }
 
 const rootElement = document.getElementById("app");
