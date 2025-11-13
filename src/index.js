fc8c76ba3b5729d8924d64f655cec4756dcef4a0
lavictc <carlos.lavictoire@gmail.com>
2025-11-13 13:23:23 +0000

    Setup initial app structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function Hi() {
-  return <p>Hi world.</p>;
-}
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
 const rootElement = document.getElementById("app");
-createRoot(rootElement).render(<Hi />);
+createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
