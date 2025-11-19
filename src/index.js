93b36a50c6f393a88f28dbf46c2d0723bc797671
lavictc <carlos.lavictoire@gmail.com>
2025-11-19 10:22:02 +0000

    confiugure redux store and wrap in app entry point

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import store from "./redux/configureStore";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("app");
+
 createRoot(rootElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
