638c0831b9a75929da174b57000aaf8932d9bd2e
lavictc <carlos.lavictoire@gmail.com>
2025-11-19 13:35:47 +0000

    Add fake api

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -20,6 +20,9 @@ module.exports = {
     headers: { "Access-Control-Allow-Origin": "*" }
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico"
